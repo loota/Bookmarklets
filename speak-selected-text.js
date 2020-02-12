959bd987dca75111baf587d7eef9411819b6b175
Heikki Naski <heikki.naski@gmail.com>
2020-02-12 21:45:56 +0200

    Create speak-selected-text.js

---
+++ speak-selected-text.js
@@ -0,0 +1,1 @@
+javascript:function getSelectionText() {     var text = "";     if (window.getSelection) {         text = window.getSelection().toString();     } else if (document.selection && document.selection.type != "Control") {         text = document.selection.createRange().text;     }     return text; } speechSynthesis.speak(new SpeechSynthesisUtterance(getSelectionText()));
